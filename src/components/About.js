a1d3835beb12918c9d421465ae4791d4f5f06bcc
Prannat Jain <prannatj@gmail.com>
2023-08-15 00:39:38 -0300

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+function About() {
+  return (
+    <div>
+      <p>
+        Computer Science student at Dalhousie University | DevOps Engineer and
+        Full Stack Developer
+      </p>
+    </div>
+  );
+}
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+      <div className="header__title">
+        <h1>I'm Prannat, Welcome to my portfolio</h1>
+      </div>
+    </header>
+  );
+}
+export default Header;
