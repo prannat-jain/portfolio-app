e1875735e4a7043fba9b3deaf46a275ef2741f28
prannat-jain <prannatj@gmail.com>
2023-08-16 00:06:11 -0300

    gh-pages setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import Header from "./components/Header";
 import About from "./components/About";
+import WorkExp from "./components/WorkExp";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <About />
-      {/*<WorkExp />
+      <WorkExp />
       <Projects />
-  <Contact />*/}
+      {/*<Contact />*/}
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,15 @@
+function Projects() {
+  return (
+    <div>
+      <h1>Here are some of my Projects</h1>
+      <div className="projectItem">
+        <h2>ScrapRadar</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+          voluptatum.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Projects;
