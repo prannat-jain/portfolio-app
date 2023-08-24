e100e2026840865d2da7230f1931e453f1707b56
prannat-jain <prannatj@gmail.com>
2023-08-24 00:49:32 -0300

    tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
-import "./App.css";
-import Header from "./components/Header";
-import About from "./components/About";
-import WorkExp from "./components/WorkExp";
-import Projects from "./components/Projects";
-
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <About />
-      <WorkExp />
-      <Projects />
-      {/*<Contact />*/}
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
